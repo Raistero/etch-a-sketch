962037db02d2aff0d18776ef5bd4b44dcd452f9e
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2024-07-10 19:43:48 +0100

    Refine the drawing functionality The canvas will only implement the drawing functionality if the mouse is being clicked and held inside the container.
    A limitation where if the cursor leaves the container, the draw
    function will still trigger upon re-entry.

--- script/script.js
+++ script/script.js
@@ -1,18 +1,47 @@
-const container = document.querySelector('.grid-container');
+const container = document.querySelector(".grid-container");
 
-container.addEventListener("mousedown", clickHandler);
+// container.addEventListener("mousedown", clickHandler);
 
-function clickHandler(e) {
+// function clickHandler(e) {
+//   let targetCell = e.target;
+//   if (targetCell.classList.contains("cell")) {
+//     targetCell.style.backgroundColor = "blue";
+//     container.addEventListener("mouseover", drawHandler);
+//     targetCell.addEventListener("mouseup", () => {
+//       container.removeEventListener("mouseover", drawHandler)
+//       console.log("mousup");
+//     });
+//   }
+// }
+
+// function drawHandler(e) {
+//   let drawCell = e.target;
+//   if (drawCell.classList.contains("cell"))
+//     drawCell.style.backgroundColor = "blue";
+// }
+
+//Variable used to track whether the mouse button is pressed or not
+//Listeners added to change this state
+let isMouseDown = false;
+let isCursorIn = true;
+
+container.addEventListener('mousedown', e => {
+  isMouseDown = true
   let targetCell = e.target;
-  if(targetCell.classList.contains('cell')){
+  if(targetCell.classList.contains('cell'))
     targetCell.style.backgroundColor = 'blue';
-    container.addEventListener('mouseover', (e) => {
-      // console.log('hey');
-      // e.target.style.backgroundColor = 'blue';
-      let drawCell = e.target;
-      if(drawCell.classList.contains('cell')) {
-        drawCell.style.backgroundColor = 'blue';
-      }
-    })
+});
+
+container.addEventListener('mouseup', () => 
+  isMouseDown = false
+);
+
+container.addEventListener('mouseover', draw);
+
+function draw(e) {
+  let targetCell = e.target;
+  if(isMouseDown){
+    if(targetCell.classList.contains('cell'))
+      targetCell.style.backgroundColor = 'blue';
   }
 }
