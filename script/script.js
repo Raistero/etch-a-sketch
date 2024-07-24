8f13ccd8b2b8aef246a8384f04a3461d5403f559
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2024-07-24 23:50:57 +0100

    Add function to change opacity with each mouseover

--- script/script.js
+++ script/script.js
@@ -7,11 +7,27 @@ const cells = document.querySelectorAll(".cell");
 let isMouseDown = false;
 let isCursorIn = true;
 
+function helperRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
+//Picks a random color and changes opacity for every mousover
+function drawHelper(cell) {
+  if (cell.style.opacity === '') {
+    if (cell.classList.contains('cell')) {
+      cell.style.backgroundColor = `rgb(${helperRGB()}, ${helperRGB()}, ${helperRGB()})`;
+      cell.style.opacity = '0.1';
+    }
+  } else {
+    cell.style.opacity = (+cell.style.opacity + 0.1).toFixed(1);
+    console.log((+cell.style.opacity + 0.1).toFixed(1));
+  }
+}
+
 container.addEventListener("mousedown", (e) => {
   isMouseDown = true;
   let targetCell = e.target;
-  if (targetCell.classList.contains("cell"))
-    targetCell.style.backgroundColor = "blue";
+  drawHelper(targetCell);
 });
 
 container.addEventListener("mouseup", () => (isMouseDown = false));
@@ -21,26 +37,25 @@ container.addEventListener("mouseover", draw);
 function draw(e) {
   let targetCell = e.target;
   if (isMouseDown) {
-    if (targetCell.classList.contains("cell"))
-      targetCell.style.backgroundColor = "blue";
+    drawHelper(targetCell);
   }
 }
 
 button.addEventListener("click", () => {
   let sizeInput = prompt("How many?");
-  container.innerHTML = '';
-  const rowGenerator = document.createElement('div');
-  const cellGenerator = document.createElement('div');
-  rowGenerator.classList.add('grid-row');
-  cellGenerator.classList.add('cell');
-  for(let i = 0; i < sizeInput; i++) {
+  container.innerHTML = "";
+  const rowGenerator = document.createElement("div");
+  const cellGenerator = document.createElement("div");
+  rowGenerator.classList.add("grid-row");
+  cellGenerator.classList.add("cell");
+  for (let i = 0; i < sizeInput; i++) {
     rowGenerator.appendChild(cellGenerator.cloneNode(true));
   }
-  for(let i = 0; i < sizeInput; i++) {
+  for (let i = 0; i < sizeInput; i++) {
     container.appendChild(rowGenerator.cloneNode(true));
   }
 
-  const generatedCells = document.querySelectorAll('.cell')
+  const generatedCells = document.querySelectorAll(".cell");
   generatedCells.forEach((cell) => {
     cell.style.width = `${(16 / sizeInput) * 60}px`;
     cell.style.height = `${(16 / sizeInput) * 60}px`;
